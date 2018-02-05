f09e3a39885b79304a9a188fbf46d810b405c9df
ozblod <cem.ozen@utexas.edu>
2018-02-05 04:50:13 +0300

    Extractor for Binance

---
+++ app/extractor/BinanceExtractor.scala
@@ -0,0 +1,30 @@
+package extractor
+
+import akka.actor.ActorSystem
+import akka.{Done, NotUsed}
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import extractor.Ticker
+import play.api.Logger
+import play.api.libs.json.{JsValue, Json}
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+
+class BinanceExtractor(val ticker: Ticker)(implicit actorSystem: ActorSystem,
+                                           mat: Materializer,
+                                           executionContext: ExecutionContext) extends Extractor {
+  private val logger = Logger(getClass)
+
+  private val baseEndpoint: String = "wss://stream.binance.com:9443/ws/"
+
+  override val tickerName: String = ticker.name
+
+  override val endpoint = ticker match {
+    case Ticker.ETHBTC => s"${baseEndpoint}ethbtc@trade"
+    case ticker => throw new IllegalArgumentException(s"Endpoint for ${ticker.name} could not be found!")
+  }
+
+  override val priceMapper: (String => JsValue) = (msg: String) => (Json.parse(msg) \ "p").get
+}
