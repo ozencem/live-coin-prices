497f7019f76e1068c4446af1b1cfd34f1d23064f
ozblod <cem.ozen@utexas.edu>
2018-02-07 02:18:02 +0300

    add buffering and throttling

--- app/extractor/Extractor.scala
+++ app/extractor/Extractor.scala
@@ -5,15 +5,16 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest, WebSocketUpgradeResponse}
-import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.{ActorMaterializer, Materializer, OverflowStrategy, ThrottleMode}
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import play.api.Logger
 import play.api.libs.json.{JsValue, Json}
 
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NonFatal
 
-trait Extractor{
+trait Extractor {
 
   private val logger = Logger(getClass)
 
@@ -36,6 +37,8 @@ trait Extractor{
         .runFold("")(_ + _)
         .flatMap(msg => Future.successful(msg))
     }.mapAsync(4) (identity)
+      .buffer(1, OverflowStrategy.dropHead)
+      .throttle(1, 500 milliseconds, 1, ThrottleMode.Shaping)
       .map(priceMapper)
       .map(p => Json.obj(tickerName -> p))
       .toMat(wsSink) (Keep.right)
@@ -45,18 +48,6 @@ trait Extractor{
       outgoing)
 
     createConnectionWithRetry(wsFlow)
-  }
-
-  def createWebsocketFlow()(implicit actorSystem: ActorSystem,
-                            materializer: Materializer,
-                            executionContext: ExecutionContext): Flow[Message, String, NotUsed] = {
-    Flow[Message].collect {
-      case TextMessage.Strict(msg) => Future.successful(msg)
-      case TextMessage.Streamed(stream) => stream
-        .limit(100)
-        .runFold("")(_ + _)
-        .flatMap(msg => Future.successful(msg))
-    }.mapAsync(4) (identity(_))
   }
 
   def createConnectionWithRetry(wsFlow: Flow[Message, Message, _])(implicit actorSystem: ActorSystem,
