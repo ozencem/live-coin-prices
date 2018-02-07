e241ecb0ae1414faaa01d578f199c881d4985cbf
ozblod <cem.ozen@utexas.edu>
2018-02-07 04:54:37 +0300

    add bittrex exchange extractor

---
+++ app/extractor/BittrexExtractor.scala
@@ -0,0 +1,21 @@
+package extractor
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.Uri
+import akka.stream.Materializer
+import play.api.Logger
+import play.api.libs.json.{JsString, JsValue, Json}
+
+import scala.concurrent.ExecutionContext
+
+class BittrexExtractor(val ticker: Ticker)(implicit actorSystem: ActorSystem,
+                                           mat: Materializer,
+                                           executionContext: ExecutionContext) extends HttpExtractor  {
+  private val logger = Logger(getClass)
+
+  override val tickerName: String = ticker.name
+
+  override val endpoint: String = "https://bittrex.com/api/v1.1/public/getticker?market=" + tickerName.split("/").reverse.mkString("-")
+
+  override val priceMapper: (String => String) = (msg: String) => (Json.parse(msg) \ "result" \ "Last").asOpt[Double].map(_.toString).getOrElse("N/A")
+}
