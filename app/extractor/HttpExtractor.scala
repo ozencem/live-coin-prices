390dcd052265ccd6d600167ed6401e28856dc46e
ozblod <cem.ozen@utexas.edu>
2018-02-07 05:50:57 +0300

    add supervision strategy to ignore buffer overflows

--- app/extractor/HttpExtractor.scala
+++ app/extractor/HttpExtractor.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.Uri.Query
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes, Uri}
-import akka.stream.Materializer
+import akka.stream.{ActorAttributes, BufferOverflowException, Materializer, Supervision}
 import akka.stream.scaladsl.{Keep, Sink, Source}
 import play.api.Logger
 import play.api.libs.json.{JsNull, JsValue, Json}
@@ -27,31 +27,39 @@ trait HttpExtractor {
   def start(wsSink: Sink[JsValue, _])(implicit actorSystem: ActorSystem,
                                       materializer: Materializer,
                                       executionContext: ExecutionContext) = {
+    val decider: Supervision.Decider = {
+      case _: BufferOverflowException => Supervision.resume
+      case _ => Supervision.stop
+    }
+
     val source = Source.tick(1 seconds, 500 milliseconds, HttpRequest(uri = endpoint))
-      .map(req => Http().singleRequest(req))
-      source.mapAsync(4) (identity)
-        .map {
-          case HttpResponse(StatusCodes.OK, _, entity, _) =>
-            entity.toStrict(100 milliseconds).map(_.data).flatMap(x => Future.successful(x.utf8String))
-          case other =>
-            logger.warn(s"http request failed, ${other}")
-            Future.successful("")
-        }
-        .mapAsync(4) (identity)
-          .alsoTo(Sink.foreach(println))
-        .map(priceMapper)
-        .statefulMapConcat { () =>
-          var lastPrice = "N/A"
-          price =>
-            if (price == lastPrice) {
-              List.empty
-            } else {
-              lastPrice = price
-              List(price)
-            }
-        }
-        .map(p => Json.obj(tickerName -> p))
-        .toMat(wsSink) (Keep.right) .run()
+    source.map(req => Http().singleRequest(req))
+      .mapAsync(4) (identity)
+      .map {
+        case HttpResponse(StatusCodes.OK, _, entity, _) =>
+          entity.toStrict(100 milliseconds).map(_.data).flatMap(x => Future.successful(x.utf8String))
+        case other =>
+          logger.warn(s"http request failed, ${other}")
+          Future.successful("")
+      }
+      .withAttributes(ActorAttributes.supervisionStrategy(decider))
+      .mapAsync(4) (identity)
+      .map(priceMapper)
+      .statefulMapConcat { () =>
+        var lastPrice = "N/A"
+        price =>
+          if (price == lastPrice) {
+            List.empty
+          } else {
+            lastPrice = price
+            List(price)
+          }
+      }
+      .map(p => Json.obj(
+        "t" -> tickerName,
+        "p" -> p,
+        "e" -> exchangeName))
+      .toMat(wsSink) (Keep.right) .run()
   }
 
   def sendReq()(implicit actorSystem: ActorSystem,
