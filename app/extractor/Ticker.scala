e21ac5eaf8e0354b5a97f11a9e13ea42072d072a
ozblod <cem.ozen@utexas.edu>
2018-02-19 06:20:42 +0300

    add new tickers

--- app/extractor/Ticker.scala
+++ app/extractor/Ticker.scala
@@ -4,20 +4,21 @@ sealed case class Ticker(name: String)
 
 object Ticker {
   object ETHBTC extends Ticker("ETH/BTC")
-  object ADABTC extends Ticker("ADA/BTC")
   object XRPBTC extends Ticker("XRP/BTC")
   object LTCBTC extends Ticker("LTC/BTC")
   object NEOBTC extends Ticker("NEO/BTC")
+  object BTGBTC extends Ticker("BTG/BTC")
+  object ETCBTC extends Ticker("ETC/BTC")
   object XMRBTC extends Ticker("XMR/BTC")
-  object XLMBTC extends Ticker("XLM/BTC")
+  object BNTBTC extends Ticker("BNT/BTC")
+  object ZECBTC extends Ticker("ZEC/BTC")
   object DASHBTC extends Ticker("DASH/BTC")
-  object REQBTC extends Ticker("REQ/BTC")
+  object QTUMBTC extends Ticker("QTUM/BTC")
+  object XVGBTC extends Ticker("XVG/BTC")
   object OMGBTC extends Ticker("OMG/BTC")
   object LSKBTC extends Ticker("LSK/BTC")
   object WAVESBTC extends Ticker("WAVES/BTC")
-  object ENGBTC extends Ticker("ENG/BTC")
-  object FUNBTC extends Ticker("FUN/BTC")
 
-  val values = Seq(ETHBTC, ADABTC, XRPBTC, LTCBTC, NEOBTC, XMRBTC, XLMBTC, DASHBTC, REQBTC,
-    OMGBTC, LSKBTC, WAVESBTC, ENGBTC, FUNBTC)
+  val values = Seq(ETHBTC, XRPBTC, LTCBTC, NEOBTC, BTGBTC, ETCBTC, XMRBTC, BNTBTC, ZECBTC, DASHBTC, QTUMBTC, XVGBTC,
+    OMGBTC, LSKBTC, WAVESBTC)
 }
